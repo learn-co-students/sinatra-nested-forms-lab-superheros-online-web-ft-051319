ba916fc597c09f0ecb68209801cbee25040b6258
Alethea Cary <aletheacary@gmail.com>
2019-07-07 18:33:09 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,14 +4,16 @@ class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
     
-    get ('/'){erb :super_hero}
+    get '/' do
+      erb :super_hero
+    end
     
     post '/teams' do 
       
       @team_name = params[:team][:name]
       @team_motto = params[:team][:motto]
-      @member1_name = params[:team][:member][0][:name]
-      @member1_power = params[:team][:member][0][:power]
+      @member1_name = params[:team][:member][][:name]
+      @member1_power = params[:team][:member][][:power]
       @member1_bio = params[:team][:member][0][:bio]
       @member2_name = params[:team][:member][1][:name]
       @member2_power = params[:team][:member][1][:power]
@@ -20,8 +22,28 @@ class App < Sinatra::Base
       @member3_power = params[:team][:member][2][:power]
       @member3_bio = params[:team][:member][2][:bio]
       
-      erb :team
+       erb :team
+      
     end
 
-
 end
+
+
+# From Valerie Moore to Everyone:  11:27 AM
+# class App < Sinatra::Base
+
+#     set :views, Proc.new { File.join(root, "../views/") }
+
+#     get '/' do
+#       erb :index
+#     end
+
+#     post "/teams" do
+#       @team = Team.new(name: params[:team][:name], motto: params[:team][:motto])
+#       members = params[:team][:members]
+#       @super_heroes = members.collect do |member_params|
+#         SuperHero.new({name: member_params[:name], power: member_params[:power], bio: member_params[:bio]})
+#       end
+#       erb :team
+#     end
+# end
